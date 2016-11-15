6591cd97b86b2f1312567f04d24216226bca1d5f
ayanna92 <Ayanna@Ayannas-MacBook-Air.local>
2016-11-15 19:23:59 +0100

    Initial Commit

---
+++ ayannacolden-pset3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ayannacolden-pset3
+//
+//  Created by Ayanna Colden on 15/11/2016.
+//  Copyright © 2016 Ayanna Colden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
